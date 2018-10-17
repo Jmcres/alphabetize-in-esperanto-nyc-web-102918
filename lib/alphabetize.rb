cb47b186ee0e67e6d4e81303a1732bbc398e7d09
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 20:29:32 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,2 @@
-def alphabetize(arr)
-  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
- arr.sort_by do |word|
-   word.split(" ").collect do |letter|
-      esperanto_alphabet.index(letter)
-end
-end 
-end
+require 'pry'
+
