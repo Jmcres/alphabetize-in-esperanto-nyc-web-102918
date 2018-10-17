82b8f00325d60171b724a3b7759d5effc39d1ca8
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 20:30:36 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,4 +3,9 @@ require 'pry'
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr) 
-  
+    arr.sort_by do |string|
+    string.split("").collect do |letter|
+        ESPERANTO_ALPHABET.index(letter)
+end 
+end 
+end 
